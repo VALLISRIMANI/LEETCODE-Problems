14424583fc22f3c1dcb66b4ff61b05f6c7f6194e
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-05-11 11:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (82.72%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+/* 
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         int result = 0;
@@ -35,5 +36,23 @@ class Solution {
         }
 
         return result;
+    }
+}
+*/
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        return dfs(root, false);
+    }
+
+    private int dfs(TreeNode node, boolean isLeft) {
+        if (node == null) return 0;
+
+        if (node.left == null && node.right == null) {
+            return isLeft ? node.val : 0;
+        }
+
+        return dfs(node.left, true) + dfs(node.right, false);
     }
 }
