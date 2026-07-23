68f49ac33fc98e2f01bd8d4100ec15b1d0123fee
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-07-23 22:15:24 +0530

    Time: 4 ms (55.25%) | Memory: 46.9 MB (14.52%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = romanMap.get(s.charAt(i));
+            if (i + 1 < s.length() && value < romanMap.get(s.charAt(i + 1))) {
+                total -= value;
+            } else {
+                total += value;
+            }
+        }
+        return total;
+    }
+}
