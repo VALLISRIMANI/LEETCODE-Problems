d191f239a8dc4dedfc109a423a2935e5a67d7391
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-08 12:34:49 +0530

    Time: 18 ms (31.72%) | Memory: 49.6 MB (55.80%) - LeetSync

---
+++ 561-array-partition/array-partition.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int result = 0;
+
+        for (int i = 0; i < n; i += 2) {
+            result += Math.min(nums[i] , nums[i + 1]);
+        }
+
+        return result;
+    }
+}
