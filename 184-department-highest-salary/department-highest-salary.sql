1f4564e783b6b3456802453c33d8613c712e77db
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-09-20 17:34:24 +0530

    Time: 1183 ms (17.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH highest_salaries AS (
+    SELECT departmentId, name, salary, 
+        DENSE_RANK() OVER(
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS rnk
+    FROM employee
+), employees_data AS (
+    SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+    FROM department d
+    JOIN highest_salaries e 
+    ON d.id = e.departmentId
+    WHERE e.rnk = 1
+)
+SELECT Department, Employee, Salary FROM employees_data;
